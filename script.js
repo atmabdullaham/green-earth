2b702a84406c69da35e553a22188ef18dc45cfac
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-09-20 15:33:29 +0600

    active button class functionality

--- script.js
+++ script.js
@@ -7,12 +7,26 @@ fetch("https://openapi.programming-hero.com/api/categories")
     )
 }
 
+
 const loadCardsBasedOnCategory = async(id)=>{
+    handleRemoveActiveClass()
+    const activeButton = document.getElementById(`button-${id}`);
+    activeButton.classList.add("active")
     const url = `https://openapi.programming-hero.com/api/category/${id}`
     const res = await fetch(url);
     const data = await res.json();
+    
+    
     displayAllPlants(data.plants)
     
+}
+
+
+const handleRemoveActiveClass = ()=>{
+    const buttons = document.getElementsByClassName("category-buttons");
+    for (const button of buttons){
+        button.classList.remove("active");
+    }
 }
 
 const showDetails = async(id)=>{
@@ -54,7 +68,7 @@ const displayCategories = (categories) =>{
         const btnDiv = document.createElement("div");
         btnDiv.classList.add("my-4")
         btnDiv.innerHTML = `
-           <button id="button-${category.id}" onclick="loadCardsBasedOnCategory(${category.id})" class = "btn w-full">${category.category_name}</button>
+           <button id="button-${category.id}" onclick="loadCardsBasedOnCategory(${category.id})" class = "btn w-full bg-transparent border-none hover:bg-green-700 hover:text-white category-buttons">${category.category_name}</button>
         `
        categoryButtonsContainer.append(btnDiv);
     }
@@ -67,15 +81,7 @@ const displayCategories = (categories) =>{
 }
 loadCategories();
 
-const handleActiveButton = (id)=>{
-    const buttons = document.getElementsByClassName("btn");
-    for (const button of buttons){
-        button.classList.remove("active-button");
-    }
-    const activeButton = document.getElementById(id);
-    activeButton.classList.add("active-button");
 
-}
 
 const loadAllTrees = async() =>{
     const url = "https://openapi.programming-hero.com/api/plants"
@@ -84,6 +90,44 @@ const loadAllTrees = async() =>{
     displayAllPlants(data.plants)
 }
 loadAllTrees()
+
+const handleAddToCart = async(plant)=>{
+    console.log(plant);
+    const cartDiv = document.getElementById("cart");
+    const url = `https://openapi.programming-hero.com/api/plant/${plant}`
+    const res = await fetch(url);
+    const data = await res.json();
+    const name = data.plants.name;
+    const price = data.plants.price;
+    calculateTotalPrice(price);
+
+    const plantDiv = document.createElement("div");
+    plantDiv.innerHTML = `
+    <div id=${data.plants.id} class="bg-green-100 p-3 rounded-lg flex justify-between items-center">
+      <div>
+         <h3 class="text-sm font-semibold">${name}</h3>
+         <h4 class="text-lg font-normal text-gray-500">৳${price} × 1</h4>
+      </div>
+      <span><i class="fa-solid fa-xmark" style="color: #f70202;"></i></span>
+    </div>
+    `
+    cartDiv.append(plantDiv)
+
+}
+
+const handleDeleteItemFromCart = (id)=>{
+    const itemToBeDeleted = document.getElementById(id);
+    itemToBeDeleted.remove();
+}
+
+const calculateTotalPrice = (price) =>{
+    console.log(price);
+    const priceSpan = document.getElementById("total-price");
+    const previousPrice = parseFloat(priceSpan.innerText);
+    const newPrice = previousPrice + price;
+    priceSpan.innerText = newPrice;
+    console.log(previousPrice);
+}
 
 const displayAllPlants = (plants)=>{
     const cardBox = document.getElementById("card-box")
@@ -104,7 +148,7 @@ const displayAllPlants = (plants)=>{
                             <span class="text-sm font-semibold text-gray-800">৳${plant.price}</span>
                           </div>
                           <div class="card-actions pt-3">
-                            <button class="btn w-full bg-green-700 rounded-full text-white text-[16px] font-medium">Add to Cart</button>
+                            <button onclick="handleAddToCart(${plant.id})" class="btn w-full bg-green-700 rounded-full text-white text-[16px] font-medium">Add to Cart</button>
                           </div>
                         </div>
         `
